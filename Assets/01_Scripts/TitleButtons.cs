e4d460498c4495597f8b2c6256a5c5d4a832b474
kevinkim910408 <kevinkim0408@naver.com>
2020-11-11 04:05:54 +0900

    Title Button Work + Instructor Scene
    1. Added Instructor Scene
    
    2. Button let player move to each scenes

---
+++ Assets/01_Scripts/TitleButtons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleButtons : MonoBehaviour
+{
+    public string StartScene;
+    public string InstructorScene;
+
+    public void OnStart()
+    {
+        SceneManager.LoadScene(StartScene);
+    }
+
+    public void OnInstructor()
+    {
+        SceneManager.LoadScene(InstructorScene);
+    }
+}
