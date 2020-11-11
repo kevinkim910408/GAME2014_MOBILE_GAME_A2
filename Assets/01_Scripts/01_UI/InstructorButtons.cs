dfe7af43c53407d1b492f1d28647c1ac08c41745
kevinkim910408 <kevinkim0408@naver.com>
2020-11-11 17:09:54 +0900

    Instructor Scene + Import Buttons
    1. Player Character Animation (UI)
    
    2. Next Page Button, Previous Page Button

--- Assets/01_Scripts/01_UI/InstructorButtons.cs
+++ Assets/01_Scripts/01_UI/InstructorButtons.cs
@@ -6,11 +6,44 @@ using UnityEngine.SceneManagement;
 public class InstructorButtons : MonoBehaviour
 {
     public string BackScene;
-    
+
+    public GameObject panel_01;
+    public GameObject panel_02;
+
+    public GameObject nextBtn;
+    public GameObject previousBtn;
+
+    private void Start()
+    {
+        panel_01.SetActive(true);
+        panel_02.SetActive(false);
+
+        nextBtn.SetActive(true);
+        previousBtn.SetActive(false);
+    }
+
 
     public void OnBack()
     {
         SceneManager.LoadScene(BackScene);
+    }
+
+    public void OnPrevious()
+    {
+        panel_01.SetActive(true);
+        panel_02.SetActive(false);
+
+        nextBtn.SetActive(true);
+        previousBtn.SetActive(false);
+    }
+
+    public void OnNext()
+    {
+        panel_01.SetActive(false);
+        panel_02.SetActive(true);
+
+        nextBtn.SetActive(false);
+        previousBtn.SetActive(true);
     }
 
   
