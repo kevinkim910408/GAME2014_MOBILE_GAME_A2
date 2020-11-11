2398578ce51cb72f5db7fb9678a45fc842f7f58e
kevinkim910408 <kevinkim0408@naver.com>
2020-11-11 17:17:43 +0900

    Title/Instructor - Inline Doc
    Title/Instructor - Inline Doc

--- Assets/01_Scripts/01_UI/InstructorButtons.cs
+++ Assets/01_Scripts/01_UI/InstructorButtons.cs
@@ -3,18 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// 
+/// Name: Junho Kim
+/// Student#: 101136986
+/// The Source file name: InstructorButtons.cs
+/// Date last Modified: 2020-11-11
+/// Program description
+///  - Managing buttons: Back to main menu button, next and previous page buttons
+///  - Set active / deactive the panels and buttons
+///  
+/// Revision History
+/// 2020-11-11: Added buttons with loading scenes and panels
+/// 
+/// </summary>
+
+
 public class InstructorButtons : MonoBehaviour
 {
+    #region Variables
+    [Header("Scene Name")]
     public string BackScene;
 
+    [Header("Panels")]
     public GameObject panel_01;
     public GameObject panel_02;
 
+    [Header("Buttons")]
     public GameObject nextBtn;
     public GameObject previousBtn;
 
+    #endregion
+
+    #region Unity_Method
     private void Start()
     {
+        // at start, only set active the panel 01 and next button.
         panel_01.SetActive(true);
         panel_02.SetActive(false);
 
@@ -22,12 +46,18 @@ public class InstructorButtons : MonoBehaviour
         previousBtn.SetActive(false);
     }
 
+    #endregion
 
+
+    #region Custom_Method
+
+    // back to main menu button
     public void OnBack()
     {
         SceneManager.LoadScene(BackScene);
     }
 
+    // set active panel page 01
     public void OnPrevious()
     {
         panel_01.SetActive(true);
@@ -36,6 +66,8 @@ public class InstructorButtons : MonoBehaviour
         nextBtn.SetActive(true);
         previousBtn.SetActive(false);
     }
+
+    // set active panel page 02 
 
     public void OnNext()
     {
@@ -46,5 +78,7 @@ public class InstructorButtons : MonoBehaviour
         previousBtn.SetActive(true);
     }
 
-  
+    #endregion
+
+
 }
--- Assets/01_Scripts/01_UI/TitleButtons.cs
+++ Assets/01_Scripts/01_UI/TitleButtons.cs
@@ -3,18 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// 
+/// Name: Junho Kim
+/// Student#: 101136986
+/// The Source file name: TitleButtons.cs
+/// Date last Modified: 2020-11-11
+/// Program description
+///  - Load Scenes with buttons from Title Scene
+///  
+/// Revision History
+/// 2020-11-11: Added Start and Instructor buttons with loading scenes
+/// 
+/// </summary>
+
 public class TitleButtons : MonoBehaviour
 {
+    #region Variables
+
+    [Header("Scene Names")]
     public string StartScene;
     public string InstructorScene;
 
+    #endregion
+
+    #region Custom_Method
+
+    // Move the scene with start button
     public void OnStart()
     {
         SceneManager.LoadScene(StartScene);
     }
 
+    // Move the scene with "i" button
     public void OnInstructor()
     {
         SceneManager.LoadScene(InstructorScene);
     }
+    #endregion
 }
