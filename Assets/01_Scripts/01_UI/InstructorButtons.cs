3737b933f22a096248d68bc0b5a3609a81947db6
kevinkim910408 <kevinkim0408@naver.com>
2020-11-11 16:33:42 +0900

    Instructor Scene
    1. Text Animation
    2. Characters Animation (UI)
    3. Back Button

---
+++ Assets/01_Scripts/01_UI/InstructorButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstructorButtons : MonoBehaviour
+{
+    public string BackScene;
+    
+
+    public void OnBack()
+    {
+        SceneManager.LoadScene(BackScene);
+    }
+
+  
+}
