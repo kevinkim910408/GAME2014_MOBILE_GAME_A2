f502a67a00adc7cba6a319fcc1af59202ef1d890
kevinkim910408 <kevinkim0408@naver.com>
2020-11-13 19:41:40 +0900

    Player Selection Work
    Selection Scene.
    
    Select player and save it with PlayerPrefs and load it from game scene

---
+++ Assets/01_Scripts/01_UI/CharacterSelection.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 
+/// Name: Junho Kim
+/// Student#: 101136986
+/// The Source file name: CharacterSelection.cs
+/// Date last Modified: 2020-11-13
+/// Program description
+///  - 
+///  
+/// Revision History
+/// 2020-11-13: 
+/// 
+/// </summary>
+/// 
+public class CharacterSelection : MonoBehaviour
+{
+    #region Variables
+
+    public GameObject[] GO;
+    public int index;
+
+    public string StartScene;
+    public string BackScene;
+
+    #endregion
+
+    #region Unity_Methods
+    private void Start()
+    {
+        index = 0;
+    }
+
+    #endregion
+
+    #region Custom_Methods
+
+    public void OnNext()
+    {
+        GO[index].SetActive(false);
+        index = (index + 1) % GO.Length;
+        //index++;
+        //if(index > GO.Length)
+        //{
+        //    index = 0;
+        //}
+        GO[index].SetActive(true);
+    }
+
+    public void OnPrevious()
+    {
+        GO[index].SetActive(false);
+        index--;
+        if(index < 0)
+        {
+            index += GO.Length;
+        }
+        GO[index].SetActive(true);
+    }
+
+    public void OnStart()
+    {
+        PlayerPrefs.SetInt("playerIndex", index);
+        SceneManager.LoadScene(StartScene);
+    }
+
+    public void OnBack()
+    {
+        SceneManager.LoadScene(BackScene);
+    }
+    #endregion
+
+    #region Trigger_Methods
+    #endregion
+}
---
+++ Assets/01_Scripts/03_Player/LoadPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// Name: Junho Kim
+/// Student#: 101136986
+/// The Source file name: AfterDieButtons.cs
+/// Date last Modified: 2020-11-13
+/// Program description
+///  - 
+///  
+/// Revision History
+/// 2020-11-13: 
+/// 
+/// </summary>
+/// 
+public class LoadPlayer : MonoBehaviour
+{
+    #region Variables
+    public GameObject[] playerPrefabs;
+    public Transform spawnPlayerPoint;
+
+    #endregion
+
+    #region Unity_Methods
+
+    private void Start()
+    {
+        int index = PlayerPrefs.GetInt("playerIndex");
+        GameObject go = 
+            Instantiate(playerPrefabs[index], spawnPlayerPoint.position, Quaternion.identity);
+    }
+    #endregion
+
+    #region Custom_Methods
+
+
+
+
+    #endregion
+
+    #region Trigger_Methods
+    #endregion
+}
