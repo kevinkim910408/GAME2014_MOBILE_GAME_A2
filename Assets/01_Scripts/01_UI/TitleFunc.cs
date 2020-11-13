eca899ff47d499b338a65f5267474d2926718c35
kevinkim910408 <kevinkim0408@naver.com>
2020-11-13 21:46:06 +0900

    Button Sounds Work
    Added sounds to all buttons

--- Assets/01_Scripts/01_UI/TitleFunc.cs
+++ Assets/01_Scripts/01_UI/TitleFunc.cs
@@ -7,17 +7,18 @@ using UnityEngine.SceneManagement;
 /// 
 /// Name: Junho Kim
 /// Student#: 101136986
-/// The Source file name: TitleButtons.cs
-/// Date last Modified: 2020-11-11
+/// The Source file name: TitleFunc.cs
+/// Date last Modified: 2020-11-13
 /// Program description
 ///  - Load Scenes with buttons from Title Scene
 ///  
 /// Revision History
 /// 2020-11-11: Added Start and Instructor buttons with loading scenes
+/// 2020-11-13: Change the class name "TitleButtons" -> "TitleFunc"
 /// 
 /// </summary>
 
-public class TitleButtons : MonoBehaviour
+public class TitleFunc : MonoBehaviour
 {
     #region Variables
 
