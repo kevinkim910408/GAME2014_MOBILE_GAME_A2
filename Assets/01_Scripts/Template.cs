60a435cdb2112d112c0b6a480dce07647e74e3a8
kevinkim910408 <kevinkim0408@naver.com>
2020-11-13 16:41:51 +0900

    Added Script Template
    Contains internal document form only

---
+++ Assets/01_Scripts/Template.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// Name: Junho Kim
+/// Student#: 101136986
+/// The Source file name: AfterDieButtons.cs
+/// Date last Modified: 2020-11-13
+/// Program description
+///  - 
+///  
+/// Revision History
+/// 2020-11-13: 
+/// 
+/// </summary>
+/// 
+public class Template : MonoBehaviour
+{
+    #region Variables
+
+
+    #endregion
+
+    #region Unity_Methods
+    #endregion
+
+    #region Custom_Methods
+
+
+
+
+    #endregion
+
+    #region Trigger_Methods
+    #endregion
+}
