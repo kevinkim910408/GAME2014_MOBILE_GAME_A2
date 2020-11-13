3598859b2a9eb62f19b906ea53d9456f2e132f05
kevinkim910408 <kevinkim0408@naver.com>
2020-11-13 16:39:28 +0900

    Player Die Work
    Player die - a panel will show up(retry, main menu buttons)

---
+++ Assets/01_Scripts/01_UI/AfterDieButtons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 
+/// Name: Junho Kim
+/// Student#: 101136986
+/// The Source file name: AfterDieButtons.cs
+/// Date last Modified: 2020-11-13
+/// Program description
+///  - After player dead, there will be 2 options with 2 buttons. - retry and back to main menu
+///  - This script is managing the buttons only
+///  
+/// Revision History
+/// 2020-11-13: Added buttons
+/// 
+/// </summary>
+/// 
+public class AfterDieButtons : MonoBehaviour
+{
+    #region Variables
+    public string retryScene;
+    public string MenuScene;
+
+    #endregion
+
+    #region Custom_Methods
+
+    public void OnRetry()
+    {
+        SceneManager.LoadScene(retryScene);
+    }
+
+    public void OnMenu()
+    {
+        SceneManager.LoadScene(MenuScene);
+    }
+
+
+    #endregion
+}
---
+++ Assets/01_Scripts/99_Temp/DieButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieButton : MonoBehaviour
+{
+    public GameObject diePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        diePanel.SetActive(false);
+    }
+
+   public void OnDie()
+    {
+        diePanel.SetActive(true);
+    }
+}
